588b0f8d5f02ee3d4d2b07f5859fbc9088aa774b
brayhanjc <brayhanjaramillo@hotmail.com>
2018-12-28 00:36:40 -0500

    Cambios en el manifest

--- __manifest__.py
+++ __manifest__.py
@@ -41,7 +41,7 @@
         'base'
     ],
     'data': [
-        'views/l10n_co_res_partner.xml'
+        'views/l10n_co_res_partner.xml',
         'data/l10n_states_co_data.xml',
         'data/l10n_cities_co_data.xml',
     ],
